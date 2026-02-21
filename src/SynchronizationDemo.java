1a3a214d8c566235f60aae3d9ebd975f73739b52
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-02-21 22:12:59 +0530

    updated

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,27 +1,27 @@
 class Table {
-    synchronized void printTable(int n) {
+    synchronized void printTable(int n, boolean newLine) {
         for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i + " ");   // trailing space required
+            System.out.print(n * i + " ");
             try {
                 Thread.sleep(400);
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
         }
-        System.out.println();
+        if (newLine) System.out.println();   // newline only for first table
     }
 }
 
 class MyThread1 extends Thread {
     Table t;
     MyThread1(Table t) { this.t = t; }
-    public void run() { t.printTable(5); }
+    public void run() { t.printTable(5, true); }
 }
 
 class MyThread2 extends Thread {
     Table t;
     MyThread2(Table t) { this.t = t; }
-    public void run() { t.printTable(100); }
+    public void run() { t.printTable(100, false); }
 }
 
 public class SynchronizationDemo {
@@ -32,7 +32,7 @@ public class SynchronizationDemo {
         MyThread2 t2 = new MyThread2(obj);
 
         t1.start();
-        t1.join();   // force order
+        t1.join();
 
         t2.start();
         t2.join();
